23e0954b1b90d6cd9cb461f657cf181c10204ddd
ely <ely@elyandpilar.com>
2021-04-14 22:35:13 -0700

    video recording, audio playing, ball spinning.  Bug right now where audio stops when video is playing

--- run.py
+++ run.py
@@ -8,15 +8,18 @@
 import RPi.GPIO as GPIO
 import time
 import subprocess
-
+import datetime
+import os
 
 # config
-DISTANCE_IN_CM = 30
+DISTANCE_IN_CM = 60
+MOTOR_SPEED = .6
 
 from enum import Enum
 class State(Enum):
     LOOKING = 1
-    PLAYING = 2
+    CLEARING = 2
+    PLAYING = 3
 
 
 # define the pins connected to L293D 
@@ -26,20 +29,20 @@ enablePin = 15
 #adc = ADCDevice() # Define an ADCDevice class object
 process = None
 
+from dataclasses import dataclass
+
+
+@dataclass
+class AppState:
+    state: State
+    process: subprocess.Popen
+    currentVideoName: str = ""
+    videoProcess: subprocess.Popen = None
 
 state = State.LOOKING
 
+
 def setupMotor():
-    # global adc
-    # if(adc.detectI2C(0x48)): # Detect the pcf8591.
-    #     adc = PCF8591()
-    # elif(adc.detectI2C(0x4b)): # Detect the ads7830
-    #     adc = ADS7830()
-    # else:
-    #     print("No correct I2C address found, \n"
-    #     "Please use command 'i2cdetect -y 1' to check the I2C address! \n"
-    #     "Program Exit. \n");
-    #     exit(-1)
     global p
     GPIO.setmode(GPIO.BOARD)   
     GPIO.setup(motoRPin1,GPIO.OUT)   # set pins to OUTPUT mode
@@ -59,30 +62,37 @@ def motor(ADC):
     if (value > 0):  # make motor turn forward
         GPIO.output(motoRPin1,GPIO.HIGH)  # motoRPin1 output HIHG level
         GPIO.output(motoRPin2,GPIO.LOW)   # motoRPin2 output LOW level
-        print ('Turn Forward...')
     elif (value < 0): # make motor turn backward
         GPIO.output(motoRPin1,GPIO.LOW)
         GPIO.output(motoRPin2,GPIO.HIGH)
-        print ('Turn Backward...')
     else :
         GPIO.output(motoRPin1,GPIO.LOW)
         GPIO.output(motoRPin2,GPIO.LOW)
-        print ('Motor Stop...')
     p.start(mapNUM(abs(value),0,128,0,100))
-    print ('The PWM duty cycle is %d%%\n'%(abs(value)*100/127))   # print PMW duty cycle.
+#    print ('The PWM duty cycle is %d%%\n'%(abs(value)*100/127))   # print PMW duty cycle.
 
-def loop():
-    while True:
-        # value = adc.analogRead(0) # read ADC value of channel 0
-        # print ('ADC Value : %d'%(value))
-        value = 255
-        motor(value)
-        time.sleep(0.2)
 
 def destroyMotor():
     p.stop()  # stop PWM
 
 
+def startVideo(appState):
+
+    now = datetime.datetime.now()
+    appState.currentVideoName = f'{now.year}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.second}'
+    print(f'video name is {appState.currentVideoName}')
+    appState.videoProcess = subprocess.Popen(['raspivid', '-o', f'{appState.currentVideoName}.h264', '-t', '30000'])
+
+def stopVideo(appState):
+    if(appState.videoProcess != None):
+        h264Name = f'{appState.currentVideoName}.h264'
+        appState.videoProcess.terminate()
+        appState.videoProcess = None
+        boxProc = subprocess.Popen(['MP4Box', '-add', h264Name, f'{appState.currentVideoName}.mp4'])
+        boxProc.wait()
+        if os.path.exists(h264Name):
+            os.remove(h264Name)
+        appState.currentVideoName = ''
 
 
 
@@ -112,50 +122,58 @@ def getSonar():     # get the measurement results of ultrasonic module,with unit
     distance = pingTime * 340.0 / 2.0 / 10000.0     # calculate distance with sound speed 340m/s 
     return distance
     
-def setup():
+def setup(appState):
     GPIO.setmode(GPIO.BOARD)      # use PHYSICAL GPIO Numbering
     GPIO.setup(trigPin, GPIO.OUT)   # set trigPin to OUTPUT mode
     GPIO.setup(echoPin, GPIO.IN)    # set echoPin to INPUT mode
+    appState.state = State.CLEARING
 
 
-def startPlaying():
-    global state
-    global process
-    print("$$ starting playing")
-    state = State.PLAYING
-    motor(255)
-    process = subprocess.Popen(['cvlc', 'disco.m4a', 'vlc://quit'])
-    print("$$ STARTED playing")
+def startPlaying(appState):
+    appState.state = State.PLAYING
+    motor(128 + 128 * MOTOR_SPEED)
+    appState.process = subprocess.Popen(['cvlc', 'disco.m4a', 'vlc://quit'])
+    time.sleep(4)
+    startVideo(appState)
 
-def stopPlaying():    
-    global state
-    state = State.LOOKING
-    print("$$ stopping playing")
+def stopPlaying(appState):    
+    appState.state = State.CLEARING
+    appState.process = None
     motor(128)
+    stopVideo(appState)
 
-def findSomeone():
+def findSomeone(appState):
     distance = getSonar() # get distance
-    print ("The distance is : %.2f cm"%(distance))
-    if (distance < DISTANCE_IN_CM):
-        startPlaying();
+    print(f'LOOKING: sonar returned distance of {distance}')
+    if (distance > 5 and distance < DISTANCE_IN_CM):
+        startPlaying(appState);
 
-def checkForEndOfSong():
-    global process
-    if(process == None):
-        stopPlaying();
+
+def startLooking(appState):
+    appState.state = State.LOOKING
+
+def waitForClear(appState):
+    distance = getSonar() # get distance
+    print(f'CLEARING: sonar returned distance of {distance}')
+    if (distance >= DISTANCE_IN_CM):
+        startLooking(appState);
+
+def checkForEndOfSong(appState):
+    if(appState.process == None):
+        stopPlaying(appState);
     else:
-        result = process.poll();
-        print(f'@@ poll returned {result}')
-        if (process.poll() != None):
-            stopPlaying();
+        result = appState.process.poll();
+        if (result != None):
+            stopPlaying(appState);
 
-def loop():
-    global state
+def loop(appState):
     while(True):
-        if(state == State.LOOKING):
-            findSomeone();
-        elif state == State.PLAYING:
-            checkForEndOfSong();
+        if(appState.state == State.LOOKING):
+            findSomeone(appState);
+        elif appState.state == State.PLAYING:
+            checkForEndOfSong(appState);
+        elif appState.state == State.CLEARING:
+            waitForClear(appState)
 
         time.sleep(.2)
         
@@ -164,12 +182,13 @@ def destroy():
     destroyMotor()
     GPIO.cleanup()
 
+appState = AppState(state=State.LOOKING,process=None)
+
 if __name__ == '__main__':     # Program entrance
-    print ('Program is starting...')
-    setup()
+    setup(appState)
     setupMotor()
     try:
-        loop()
+        loop(appState)
     except KeyboardInterrupt:  # Press ctrl-c to end the program.
         destroy()
 
