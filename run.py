5be62aadccfe401f5ca1e3ab565eface9e1cbce9
ely <ely@elyandpilar.com>
2021-04-17 11:02:11 -0700

    moved sonar into its own separate file

--- run.py
+++ run.py
@@ -1,76 +1,31 @@
 #!/usr/bin/env python3
 
+from dataclasses import dataclass
+from threading import Thread
+import disco
 import RPi.GPIO as GPIO
 import time
-from threading import Thread
 from disco_machine import DiscoMachine
-import disco
-from dataclasses import dataclass
+from sonar import Sonar
 
 ################################################################################################
 # 
 # Model and State
 #
 
+DISTANCE_IN_CM = 60
+
 @dataclass
 class AppState:
     machine: DiscoMachine = None
-
-
-################################################################################################
-# 
-# Sonar
-#
-
-
-# config
-
-DISTANCE_IN_CM = 60
-trigPin = 16
-echoPin = 18
-MAX_DISTANCE = 220          # define the maximum measuring distance, unit: cm
-timeOut = MAX_DISTANCE*60   # calculate timeout according to the maximum measuring distance
-
-def pulseIn(pin,level,timeOut): # obtain pulse time of a pin under timeOut
-    t0 = time.time()
-    while(GPIO.input(pin) != level):
-        if((time.time() - t0) > timeOut*0.000001):
-            return 0;
-    t0 = time.time()
-    while(GPIO.input(pin) == level):
-        if((time.time() - t0) > timeOut*0.000001):
-            return 0;
-    pulseTime = (time.time() - t0)*1000000
-    return pulseTime
-    
-def getSonar():     # get the measurement results of ultrasonic module,with unit: cm
-    GPIO.output(trigPin,GPIO.HIGH)      # make trigPin output 10us HIGH level 
-    time.sleep(0.00001)     # 10us
-    GPIO.output(trigPin,GPIO.LOW) # make trigPin output LOW level 
-    pingTime = pulseIn(echoPin,GPIO.HIGH,timeOut)   # read plus time of echoPin
-    distance = pingTime * 340.0 / 2.0 / 10000.0     # calculate distance with sound speed 340m/s 
-    return distance
-    
-
-
-def setupSonar(appState):
-    GPIO.setmode(GPIO.BOARD)      # use PHYSICAL GPIO Numbering
-    GPIO.setup(trigPin, GPIO.OUT)   # set trigPin to OUTPUT mode
-    GPIO.setup(echoPin, GPIO.IN)    # set echoPin to INPUT mode
+    sonar: Sonar = None
 
 def findSomeone(appState):
-    distance = getSonar() # get distance
+    distance = appState.sonar.get() # get distance
     if(appState.machine.state == disco.State.LOOKING):
         print(f'LOOKING: sonar returned distance of {distance}')
     if (distance > 5 and distance < DISTANCE_IN_CM):
         appState.machine.addEvent(disco.Events.PersonApproaching)
-
-
-# def waitForClear(appState):
-#     distance = getSonar() # get distance
-#     print(f'CLEARING: sonar returned distance of {distance}')
-#     if (distance >= DISTANCE_IN_CM):
-#         appState.machine.startLooking();
 
 
 ################################################################################################
@@ -80,8 +35,9 @@ def findSomeone(appState):
 
 def setup(appState):
     appState.machine = DiscoMachine(disco.Features(video=False, music=True))
-    setupSonar(appState)
     appState.machine.setup()
+    appState.sonar = Sonar()
+    appState.sonar.setup()
 
 def loop(appState):
     while(True):
