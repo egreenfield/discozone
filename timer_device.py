4f7aa4629f1af0ff6daac093e4ba98fa14e5cff2
ely <ely@elyandpilar.com>
2021-05-20 20:32:20 -0700

    added missing timer file

---
+++ timer_device.py
@@ -0,0 +1,30 @@
+import threading
+import devices
+
+class TimerEvent:
+    TIMER_COMPLETE = "Timer:Complete"
+
+class TimerCommand:
+    START = "Timer:start"
+
+class TimerDevice(devices.Device):
+
+    def __init__(self):
+        devices.Device.__init__(self)
+        self.duration = 10
+
+    def setConfig(self,config):
+        devices.Device.setConfig(self,config)
+        if('duration' in config):
+            self.duration = config['duration']
+
+
+    def start(self):
+        threading.Timer(self.duration, lambda : self.trigger()).start()
+
+    def trigger(self):
+        self.raiseEvent(TimerEvent.TIMER_COMPLETE)
+
+    def onCommand(self,cmd,data = None):
+        if(cmd == TimerCommand.START):
+            self.start()
